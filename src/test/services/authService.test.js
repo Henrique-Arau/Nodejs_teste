36ad2a248652ac7767406e048f6309823a4623f0
Henrique-Arau <penielh43@gmail.com>
2023-12-18 17:35:57 -0300

    Testes de Cadastros

--- src/test/services/authService.test.js
+++ src/test/services/authService.test.js
@@ -1,5 +1,7 @@
 import { describe, it } from '@jest/globals';
+import bcryptjs from 'bcryptjs';
 import AuthService from '../../services/authService';
+import Usuario from '../../models/usuario';
 
 const authService = new AuthService();
 
@@ -18,5 +20,71 @@ describe('Testando a authService.cadastrarUsuario', () => {
     // em seguida, o ASSERT será utilizado para validar se as
     // informações retornadas, ou os erros lançados, estavam de
     // acordo com o esperado.
+  });
+
+  it('- A senha do usuario precisa ser criptografada quando for salva no banco de dados', async () => {
+    // Arrange
+    const data = {
+      nome: 'John Doe',
+      email: 'johndoe@example.com',
+      senha: 'senha123',
+    };
+
+    // ACT
+    const resultado = await authService.cadastrarUsuario(data);
+    const senhaIguais = await bcryptjs.compare('senha123', resultado.content.senha);
+
+    // Assert
+    expect(senhaIguais).toStrictEqual(true);
+    await Usuario.excluir(resultado.content.id);
+  });
+
+  it('- Não pode ser cadastrado um usuario com email duplicado', async () => {
+    // Arrange
+    const usuarioMock = {
+      nome: 'Raphael',
+      email: 'teste@gmail.com',
+      senha: '123456',
+    };
+
+    // ACT
+    const usuarioSave = authService.cadastrarUsuario(usuarioMock);
+
+    // Assert
+    await expect(usuarioSave).rejects.toThrowError('O email já esta cadastrado!');
+  });
+
+  it('- Ao cadastrar um usuario deve ser retornado uma mensagem informando que o usuario foi cadastrado', async () => {
+    // Arrange
+    const data = {
+      nome: 'Johm Doe',
+      email: 'johndoe@example.com',
+      senha: 'senha123',
+    };
+
+    // ACT
+    const resultado = await authService.cadastrarUsuario(data);
+
+    // Assert
+    expect(resultado.message).toEqual('usuario criado');
+
+    await Usuario.excluir(resultado.content.id);
+  });
+
+  it('Ao cadastrar um usuário, validar o retorno das informações do usuário', async () => {
+    // Arrange
+    const data = {
+      nome: 'John Doe',
+      email: 'johndoe@example.com',
+      senha: 'senha123',
+    };
+
+    // ACT
+    const resultado = await authService.cadastrarUsuario(data);
+
+    // Assert
+    expect(resultado.content).toMatchObject(data);
+
+    await Usuario.excluir(resultado.content.id);
   });
 });
